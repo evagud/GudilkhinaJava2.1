8a114e78619f91a9250b6c34a671cfc8f5c9014f
Evgenia Gudilkhina <evagud22.4@gmail.com>
2020-05-07 17:41:59 +0400

    added Java/

---
+++ Java/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int balance = 2_000_000_000;
+        int replen =  500_000_000;
+        int total = balance + replen;
+        System.out.println(total);
+    }
+}
